a2a79316cdb7c5e776de7ce64fe21724af9ab337
Chris Fotso <chrisfotso@gmail.com>
2019-05-12 14:46:54 -0500

    Created file for API routing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const quoteRouter = require('./routes/quotesAPI');
+
 const port = process.env.port || 5000;
 
 app.use(express.json());
 
+//Redirecting all API calls to use the API router
+app.use('/api/quotes', quoteRouter);
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
