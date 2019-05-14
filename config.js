acb8fc03ca81edfe74ed83f7db612a894a3a03ca
Chris Fotso <chrisfotso@gmail.com>
2019-05-14 14:08:10 +0000

    Testing git ignore

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB_URL: 'mongodb+srv://chris:Gitthefuckoutabbey23%21@cluster0-qiss7.mongodb.net/test?retryWrites=true'
+    DB_URL: 'mongodb+srv://chris:Gitthefuckoutabbey23%21@cluster0-qiss7.mongodb.net/test?retryWrites=true',
+    test: 'testing gitignore'
 }
