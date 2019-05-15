e5ef3b622bde8087403d2b8774ddfb4473b76591
Chris Fotso <chrisfotso@gmail.com>
2019-05-14 19:12:11 -0500

    Added comments to quotes router

--- routes/quotesAPI.js
+++ routes/quotesAPI.js
@@ -8,6 +8,8 @@ const {
   getRandomIndex
 } = require('./functions/quotes')
 
+//METHOD: POST
+//DESCRIPTION: Adds quote to Mongo database
 router.post('/', async (req, res) => {
   const {
     quoteId,
@@ -39,10 +41,15 @@ router.post('/', async (req, res) => {
   }
 })
 
+//METHOD: GET
+//DESCRIPTION: Returns all quotes
 router.get('/', (req, res) => {
   res.status(200).send(dummyQuotes);
 })
 
+//METHOD: GET
+//DESCRIPTION: Returns one quote with the specified id number
+//PARAMETER 'id': id of the quote you want to get
 router.get('/id/:id', (req, res) => {
   const resultArr = dummyQuotes.filter(quote => quote.id === Number(req.params.id));
 
@@ -55,6 +62,10 @@ router.get('/id/:id', (req, res) => {
   return res.status(200).send(resultArr[0]);
 })
 
+//METHOD: GET
+//DESCRIPTION: Returns a specified number of random quotes
+//PARAMETER 'num': number of random quotes you want to get
+//                 if parameter 'num' is left out, one random quote is returned
 router.get('/random/qty/:num?', (req, res) => {
   const [smallestId, largestId] = [0, dummyQuotes.length - 1];
   const result = [];
@@ -78,6 +89,9 @@ router.get('/random/qty/:num?', (req, res) => {
           : res.status(200).send(result[0]);
 })
 
+//METHOD: GET
+//DESCRIPTION: Returns one random quote from a specified character
+//QUERY PARAMETER 'name': name of whatever character you want a random quote from
 router.get('/random/character', (req, res) => {
   const {name} = req.query
 
@@ -89,6 +103,9 @@ router.get('/random/character', (req, res) => {
   return res.status(200).send(filteredQuotes[randomQuoteIndex]);
 })
 
+//METHOD: GET
+//DESCRIPTION: Returns all quotes from specified character
+//QUERY PARAMETER 'name': name of whatever character you want a random quote from
 router.get('/character', (req, res) => {
   const {name} = req.query
   
